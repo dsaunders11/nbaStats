d2c62a0508afd0b64df01de27417872597c29a4b
David <d.saunders@yale.edu>
2021-12-07 15:28:04 -0500

    Update operate.py

--- operate.py
+++ operate.py
@@ -25,9 +25,9 @@ if len(player) > 0:
 
     training_data = compiler(pl.stats)
     elapsed.progress(40)
-    # statistics = compiler(pl.stats)
+    statistics = compiler(pl.stats, show=0)
     elapsed.progress(60)
-    plstats = training_data[['date', 'pts', 'reb', 'ast']]
+    plstats = statistics[['date', 'pts', 'reb', 'ast']]
 
     pred_model, pred_inputs = train_model(training_data)
     pred_modelfr, pred_inputsfr = training_forest(training_data)
