60a00c98a034d8287960c57f5541dd31138baf2d
David <d.saunders@yale.edu>
2021-12-09 18:34:55 -0500

    org

--- nbaStats/forest.py
+++ nbaStats/forest.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.ensemble import RandomForestRegressor
 
-from nbastats.train import inpout
+from nbaStats.train import inpout
 
 #https://www.geeksforgeeks.org/random-forest-regression-in-python/
 
